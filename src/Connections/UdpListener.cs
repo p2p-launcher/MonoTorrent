67b41a0d4a133973ff9e4b78773cf2f426bf328a
Jonas Laursen <joamla96@gmail.com>
2025-11-18 13:28:57 +0100

    Updated to dotnet 8

--- src/Connections/UdpListener.cs
+++ src/Connections/UdpListener.cs
@@ -54,7 +54,7 @@ namespace MonoTorrent.Connections
                 throw new InvalidOperationException ($"The listener could not bind to ${LocalEndPoint}. Choose a new listening endpoint.");
             if (Status == ListenerStatus.NotListening || Client == null)
                 throw new InvalidOperationException ("You must invoke StartAsync before sending or receiving a message with this listener.");
-            await Client.SendAsync (buffer, buffer.Length, endpoint).ConfigureAwait (false);
+            await Client.SendAsync (buffer.ToArray(), buffer.Length, endpoint).ConfigureAwait (false);
         }
 
         protected override void Start (CancellationToken token)
